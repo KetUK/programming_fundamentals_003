981bfefdee2e51ab3b00c8f4e95e2ddd1a3812e0
Ketaki S <ketaki.k2525@gmail.com>
2020-02-26 14:08:49 +0000

    code for programming fundamentals 003

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -1,5 +1,6 @@
 const catalogueService = require("../app/catalogue_service");
 
+
 describe("catalogueService", () => {
   describe("catalogueService.countBooksByAuthor", () => {
     test("returns the total number of books written by the given author", () => {
@@ -8,4 +9,54 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
+ });
+
+ describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(
+      catalogueService.checkBookByTitle(
+        "The Assassination of Margaret Thatcher"
+      )
+    ).toBe(true);
+  });
 });
+
+test("returns true if the book exists", () => {
+  expect(
+    catalogueService.checkBookByTitle("The Chronicles of Narnia")
+  ).toBe(false);
+});
+
+describe("catalogueService.countBooksByFirstLetter", () => {
+  test("returns count of Books by First Letter for W", () => {
+    expect(catalogueService.countBooksByFirstLetter("W")).toBe(2);
+  });
+});
+
+describe("catalogueService.getQuantity", () => {
+  test("returns quantity for given books", () => {
+    expect(catalogueService.getQuantity("A Place of Greater Safety")).toBe(
+      11
+    );
+  });
+});
+
+describe("catalogueService.getBooksByAuthor", () => {
+  test("returns all the books by the given author", () => {
+    expect(catalogueService.getBooksByAuthor("Robert Bolaño")).toEqual([
+      { title: "2666", author: "Robert Bolaño", quantity: 17 },
+      { title: "Night In Chile", author: "Robert Bolaño", quantity: 8 }
+    ]);
+  });
+});
+
+describe("catalogueService.checkQuantity", () => {
+  test("returns true for given title By Night In Chile and quantity 4", () => {
+    expect(catalogueService.checkQuantity("Night In Chile", 4)).toBe(true);
+  });
+
+  test("returns true for given title By Night In Chile and quantity 100", () => {
+    expect(catalogueService.checkQuantity("Night In Chile", 100)).toBe(false);
+  });
+});
+
